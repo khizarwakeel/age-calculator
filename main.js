19f9751b4ee3d6fea263e5adbc1de89d86322f3f
Khizar Wakeel <khizarwakeel@gmail.com>
2024-05-25 23:48:25 +0500

    added age calculator

---
+++ main.js
@@ -0,0 +1,20 @@
+let inputField = document.getElementById("inputField");
+var currentDate = new Date();
+let messageHeading = document.getElementById("messageHeading")
+
+const ageCal = () => {
+
+    let ageValue = inputField.value;
+    let ageValueDate = new Date(ageValue);
+
+    if (isNaN(ageValueDate.getTime())) {
+        messageHeading.innerHTML = `<span class="text-danger">Please enter a valid date format (e.g., YYYY-MM-DD).</span>`;
+        return;
+    }
+
+    let yearDiffMilli = currentDate - ageValueDate;
+    let yearDiff = yearDiffMilli / (1000 * 60 * 60 * 24 * 365);
+    let result = Math.floor(yearDiff)
+    messageHeading.innerHTML = `You are <span class="text-danger">${result} Years </span>old!`;
+
+}
